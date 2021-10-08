8855e46b77eae05bdc9f882a68a298825723311c
Code Sandbox <hello@codesandbox.io>
2021-10-08 00:20:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.03ea5a1908e27d1da71f52090ad3058c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513aefa8e9c957b58810d6123ccb49c0",
+    "url": "https://huy-developer-code.github.io/csb-oq6zz/index.html"
+  },
+  {
+    "revision": "3ad4b57ec28ef442961f",
+    "url": "https://huy-developer-code.github.io/csb-oq6zz/static/css/main.2a761bcc.chunk.css"
+  },
+  {
+    "revision": "e616ded9a93d40a0b2e4",
+    "url": "https://huy-developer-code.github.io/csb-oq6zz/static/js/2.74cb6352.chunk.js"
+  },
+  {
+    "revision": "3ad4b57ec28ef442961f",
+    "url": "https://huy-developer-code.github.io/csb-oq6zz/static/js/main.77b42353.chunk.js"
+  },
+  {
+    "revision": "db4f3bc8dd19a86bd5ff",
+    "url": "https://huy-developer-code.github.io/csb-oq6zz/static/js/runtime~main.9a6213a1.js"
+  }
+]);
